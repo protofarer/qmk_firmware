daa1ac4930257420fb3b3df1264bf7f8912ddbb1
Kenny Baron <kennybaron@fastmail.com>
2023-06-25 14:30:36 -0400

    set quick_tap_term_per_key for lctl_t(kc_esc)

--- keyboards/keychron/s1/ansi/white/keymaps/kennyv1/keymap.c
+++ keyboards/keychron/s1/ansi/white/keymaps/kennyv1/keymap.c
@@ -28,13 +28,23 @@ enum layers{
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 #define HOLD_ON_OTHER_KEY_PRESS
+#define QUICK_TAP_TERM_PER_KEY
+
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LCTL_T(KC_ESC):
+            return QUICK_TAP_TERM - 125;
+        default:
+            return QUICK_TAP_TERM;
+    }
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [PROGRAMMING_BASE] = LAYOUT_75_ansi(
         KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,   KC_PSCR,  KC_DEL,   BL_STEP,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,   KC_EQL,   KC_BSPC,            KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LPRN,   KC_RPRN,  KC_BSLS,            KC_PGDN,
-        MT(MOD_LCTL, KC_ESC), KC_A,KC_S,KC_D,   KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,             KC_ENT,             KC_HOME,
+        LCTL_T(KC_ESC), KC_A,KC_S,KC_D,   KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,             KC_ENT,             KC_HOME,
         KC_LSFT, KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,             RSFT_T(KC_UNDS),  KC_UP,    KC_END,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
